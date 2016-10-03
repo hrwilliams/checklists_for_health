529e65c3df4bdbcff24098820d1a53bd77157021
Halle Williams <williams.hr@gmail.com>
2016-10-03 00:05:54 -0700

    fix ajax on physical task

--- app/controllers/physical_tasks_controller.rb
+++ app/controllers/physical_tasks_controller.rb
@@ -8,10 +8,39 @@ class PhysicalTasksController < ApplicationController
     @week = Week.find(params[:week_id])
     @physical_task = @week.physical_tasks.new(physical_task_params)
     if @physical_task.save
-      redirect_to week_path(@physical_task.week)
+      respond_to do |format|
+        format.html { redirect_to week_path(@physical_task.week) }
+        format.js
+      end
     else
       render :new
     end
+  end
+
+  def edit
+    @week= Week.find(params[:week_id])
+    @physical_task = PhysicalTask.find(params[:id])
+  end
+
+  def update
+    @week= Week.find(params[:week_id])
+    @physical_task = PhysicalTask.find(params[:id])
+    if @physical_task.update(physcical_task_params)
+      flash[:notice] = "Task successfully updated!"
+      respond_to do |format|
+        format.html { redirect_to week_path(@physical_task.week) }
+        format.js
+      end
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @physical_task = PhysicalTask.find(params[:id])
+    @physical_task.destroy
+    flash[:notice] = "Task successfully deleted."
+    redirect_to week_path(@physical_task.week)
   end
 
   private
